9e9488a8494c61c31ae0448b23db4fab2e438a1c
Monica <dg.monica.bustamante@gmail.com>
2024-06-28 08:30:45 -0300

    add bottons

--- menu.php
+++ menu.php
@@ -2,7 +2,8 @@
 echo '<ul>
 <li><a href="index_php.php">Inicio</a></li>
 <li><a href="empresa.php">Empresa</a></li>
-<li><a href="#catalogo">Catálogo</a></li>
+<li><a href="catalogo_dama.html">Catálogo Dama</a></li>
+<li><a href="catalogo_caballero.html">Catálogo Caballero</a></li>
 <li><a href="#contacto">Contacto</a></li>
 </ul>'
 ?>
